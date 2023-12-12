fcd99ee7287244f6575bed7881a92246c0c5651b
Rick South <rick@natamus.com>
2023-12-12 09:42:48 +0100

    Mineral Chance source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/mineralchance/neoforge/events/NeoForgeMiningEvent.java
@@ -0,0 +1,21 @@
+package com.natamus.mineralchance.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.mineralchance.events.MiningEvent;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeMiningEvent {
+	@SubscribeEvent
+	public static void onBlockBreak(BlockEvent.BreakEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		MiningEvent.onBlockBreak(level, e.getPlayer(), e.getPos(), e.getState(), null);
+	}
+}
